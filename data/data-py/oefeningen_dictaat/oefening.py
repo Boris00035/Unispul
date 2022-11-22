f1a22193c7ad813d70d50dd01a9b612b9ecbb0c1
Boris00035 <borisvanboxtel@gmail.com>
2022-11-22 11:55:58 +0100

    datapy en lial

---
+++ data/data-py/oefeningen_dictaat/oefening.py
@@ -0,0 +1,57 @@
+# # opdracht 2.1.1
+# def ex_or(input1, input2):
+    # if (input1 or input2) and not (input1 and input2):
+#         return True
+#     else:
+#         return False
+
+# print(ex_or(True,True))
+# print(ex_or(True,False))
+# print(ex_or(False,True))
+# print(ex_or(False,False))
+
+# opdracht 2.3.1
+# import math
+# input = int(input("Geef een positief getal: "))
+
+# if input >= 0:
+#     print(math.sqrt(input))
+
+# # opdracht 2.4.1
+# lijst1 = list(range(0, 99))
+# lijst3 = []
+
+# for n in lijst1:
+#     lijst3.append(lijst1[n] * 100 - lijst1[n]**2) 
+
+# print(lijst3)
+
+# # opdracht 2.4.2
+# lijst_celcius = [7, 24, 19, 88, 79, 3.35, -10]
+
+# for n in range(0, len(lijst_celcius)):
+#     if lijst_celcius[n] <= 0:
+#         print("Fahrenheit: ", lijst_celcius[n] * (9/5) + 32, "het vriest")
+#     else:
+#         print("Fahrenheit: ", lijst_celcius[n] * (9/5) + 32, "het vriest niet")
+
+# # opdracht 2.5.1
+# a = 1
+# b = 1
+# print(a)
+# print(b)
+# for n in range(0, 100):
+#     c=a+b 
+#     print(c)
+#     a=b
+#     b=c
+    
+
+
+
+
+
+
+
+
+    
