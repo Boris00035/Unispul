dc5a2f53c2243481906e00e36bda098e8b0660f6
Boris00035 <borisvanboxtel@gmail.com>
2022-12-09 17:03:07 +0100

    bruhletterlijknooitmeer

--- data/data-p/labJournaal/grafiek.py
+++ data/data-p/labJournaal/grafiek.py
@@ -1,7 +1,47 @@
 import matplotlib.pyplot as plt
 import math
 
-ingangshoekfrequentie = [30 * 2*math.pi
+ingangshoekfrequentie = [
+10*2*math.pi
+,20*2*math.pi
+,30*2*math.pi
+,40*2*math.pi
+,50*2*math.pi
+,60*2*math.pi
+,70*2*math.pi
+,80*2*math.pi
+,90*2*math.pi
+,100*2*math.pi
+,110*2*math.pi
+,120*2*math.pi
+,130*2*math.pi
+,140*2*math.pi
+,150*2*math.pi
+,160*2*math.pi
+,170*2*math.pi
+,180*2*math.pi
+,190*2*math.pi
+,200*2*math.pi
+,210*2*math.pi
+,220*2*math.pi
+,230*2*math.pi
+,240*2*math.pi
+,250*2*math.pi
+,260*2*math.pi
+,270*2*math.pi
+,280*2*math.pi
+,290*2*math.pi
+,300*2*math.pi
+,310*2*math.pi
+,320*2*math.pi
+,330*2*math.pi
+,340*2*math.pi
+,350*2*math.pi
+,360*2*math.pi
+,370*2*math.pi
+]
+
+ingangshoekfrequentie1 = [30 * 2*math.pi
 ,40*2*math.pi
 ,50*2*math.pi
 ,60*2*math.pi
@@ -64,8 +104,8 @@ for n in ingangshoekfrequentie:
     theoretischefaserespons.append(math.atan(-n * R * C))
 
 plt.xscale('log')
-plt.plot(ingangshoekfrequentie, faserespons)
-plt.plot(ingangshoekfrequentie, theoretischefaserespons)
+# plt.plot(ingangshoekfrequentie1, faserespons)
+plt.plot(ingangshoekfrequentie, theoretischeamplituderespons)
 
 
 plt.ylabel('phi(w)')
